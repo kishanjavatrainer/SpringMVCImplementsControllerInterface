533a7ea698e3080a68bb58d662772472b986a786
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-06-18 07:35:19 +0530

    source code

---
+++ SpringMVCImplementsControllerInterfaceWebApp/src/com/infotech/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.infotech.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloWorldController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request,HttpServletResponse response) throws Exception {
+		return new ModelAndView("main", "message", "Welcome to Spring MVC world!!!");
+	}
+
+}
